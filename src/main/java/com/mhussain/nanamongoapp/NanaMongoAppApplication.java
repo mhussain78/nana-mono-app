d988c99e5cd9620034b83526ea9d34c7567c8562
Mohamad Hussian <inf.mhussain@googlemail.com>
2024-03-24 07:36:09 +0100

    init version

---
+++ src/main/java/com/mhussain/nanamongoapp/NanaMongoAppApplication.java
@@ -0,0 +1,13 @@
+package com.mhussain.nanamongoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanaMongoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanaMongoAppApplication.class, args);
+	}
+
+}
